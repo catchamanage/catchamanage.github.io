04b148946598bcfb2401fa5473353d659b640454
hiennv6655-seta <nvhien129@gmail.com>
2017-07-23 10:49:36 +0700

    change

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,8 +1,8 @@
 // Cache names
 
-var dataCacheName = 'catchaData-v1.1.1'
+var dataCacheName = 'catchaData-v1.1.2'
 
-var cacheName = 'catchaPWA-1.1'
+var cacheName = 'catchaPWA-1.2'
 
 // Application shell files to be cached
 
