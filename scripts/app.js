333e5e13de9291eea43a8a59addefd7b81d8292f
hiennv6655-seta <nvhien129@gmail.com>
2017-08-11 09:05:09 +0700

    app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 if ('serviceWorker' in navigator) {
-     navigator.serviceWorker
-             .register('./serviceWorker.js')
-             .then(function() 
-                    { console.log('Service Worker Registered'); 
-             });
+	navigator.serviceWorker
+		.register('./serviceWorker.js')
+	    .then(function(reg) {
+	    	subscribe(reg);
+	    	console.log('Service Worker Registered'); 
+	    });
+}
+
+function subscribe(serviceWorkerReg) {  
+    serviceWorkerReg.pushManager.subscribe({userVisibleOnly: true})
 }
